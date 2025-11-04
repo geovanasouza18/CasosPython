925367f193b90403efc52c0bb008468a5d4ddea7
Geovana Souza <geovanasouza0712@gmail.com>
2025-11-04 15:25:46 -0300

    Atualizações

--- caso015.py
+++ caso015.py
@@ -1,9 +1,26 @@
 import time
-nome = str(input('Digite seu nome: '))
+full_name = str(input('Digite seu nome completo: '))
+first_name = str(input('Digite seu primeiro nome: '))
+middle_name = str(input('Digite seu nome do meio (caso tenha): '))
+last_name = str(input('Digite seu último nome: '))
+nickname = str(input('Digite seu nickname: '))
+occupation = str(input('Digite sua profissão: '))
+origin = str(input('Digite sua nacionalidade: '))
+marital_status = str(input('Digite seu estado civil: '))
 idade = int(input('Digite sua idade: '))
 altura = float(input('Digite sua altura: '))
 
 meses = idade * 12
 time.sleep(2)
-print(f'O nome do suspeito(a) é {nome}, tem {idade} anos e sua altura é de {altura} metros')
-print(f'Sua idade em meses é {meses}')
+print('-' * 30)
+print(f'Seu nome completo: {full_name}\n'
+      f'Seu primeiro nome: {first_name}\n'
+      f'Seu nome do meio: {middle_name}\n'
+      f'Seu último nome: {last_name}\n'
+      f'Seu nickname: {nickname}\n'
+      f'Sua ocupação: {occupation}\n'
+      f'Sua origem: {origin}\n'
+      f'Seu estado civil: {marital_status}\n'
+      f'Sua idade: {idade}\n'
+      f'Sua idade em meses: {meses}\n'
+      f'Sua altura: {altura}\n')
