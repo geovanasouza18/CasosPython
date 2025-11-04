d14bb91c3b35a065ebe800beaf87d6835d07b1cd
Geovana Souza <geovanasouza0712@gmail.com>
2025-11-04 08:20:43 -0300

    caso novo

---
+++ caso014.py
@@ -0,0 +1,8 @@
+name = 'aLiCe'
+city = 'rIo De JaNeIrO'
+
+SUSPECT_UPPER = name.upper()
+CITY_LOWER = city.lower()
+print(f'SUSPECT_UPPER {SUSPECT_UPPER}')
+print(f'CITY_LOWER {CITY_LOWER}')
+print(f'REPORT {SUSPECT_UPPER} - from - {CITY_LOWER}')
